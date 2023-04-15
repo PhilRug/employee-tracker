ffa5d4c8c330a8cb8a6dd679953894db23f351dc
PhilRug <ruggiero.pj@gmail.com>
2023-04-15 18:00:15 -0400

    options for node

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ const options = [
     "Update Employee Role",
     "Quit",
 ];
+//start everything
+async function launch() {
+    answers = await inquirer.prompt([{
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choices',
+        choices: options
+    }]);
+//choices in node
+    switch(answers.choices) {
+        case options[0]: viewDepartments();
+        break;
+        case options[1]: viewRoles();
+        break;
+        case options[2]: viewEmployees();
+        break;
+        case options[3]: addDepartments();
+        break;
+        case options[4]: addRole();
+        break;
+        case options[5]: addEmployee();
+        break;
+        case options[6]: updateRole();
+        break;
+        case options[7]: quit();
+        break;
+    }
+}
