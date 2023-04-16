0841fb12373890653852c7ccb209b8ac52c05432
PhilRug <ruggiero.pj@gmail.com>
2023-04-16 15:36:59 -0400

    add employee function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const EmployeeDb = require('./db/')
+
 //options for node
 const options = [
     "View all Deparments", 
@@ -11,6 +12,7 @@ const options = [
     "Update Employee Role",
     "Quit",
 ];
+
 //start everything
 async function launch() {
     answers = await inquirer.prompt([{
@@ -19,6 +21,7 @@ async function launch() {
         name: 'choices',
         choices: options
     }]);
+
 //choices in node
     switch(answers.choices) {
         case options[0]: viewDepartments();
@@ -84,24 +87,8 @@ async function addDepartment() {
 
 //add role in employee_db
 async function addRole() {
-    const answer = await inquirer.prompt ([{
-        name: 'name',
-        type: 'input',
-        message: 'Enter Role Name',
-        validate: (name) => {return name != ''}
-    }]);
-    const sql = 'INSERT INTO department SET ?';
-    await db.query(sql,
-        {
-            name: answers.name
-        }
-    );
-    console.log('\nAdded department ' + answer.name + ' to the database\n');
-    launch();
-
-}
-//add employee in employee_db
-async function addEmployee() {
+    const departments = await db.query(`SELECT id, name FROM department`);
+    const dept_list = departments.map(function (x) { return x.name });
     const answer = await inquirer.prompt ([
         {
         name: 'name',
@@ -123,15 +110,22 @@ async function addEmployee() {
         name: 'choice',
         type: 'list',
         message: 'Enter a department for this role:',
-        choices: ''
+        choices: dept_list
         }
 ]);
     const sql = 'INSERT INTO department SET ?';
     await db.query(sql,
         {
-            name: answers.name
+            title: answers.name,
+            salary: parseFloat(answers.salary),
+            department_id: getRecordId(departments, 'name', answers.choice)
         }
     );
-    console.log('\nAdded department ' + answer.name + ' to the database\n');
+    console.log('\nAdded Role ' + answer.name + ' to the database\n');
     launch();
+}
+//add employee in employee_db
+async function addEmployee() {
+    const roless = await db.query(`SELECT id, name FROM role`);
+    const role_list = roles.map(function (x) { return x.name });
 }
